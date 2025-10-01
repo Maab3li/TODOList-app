107b4b042c0c5c62278cb9edef7f69e4ea028837
Maab3li <maab3li555@gmail.com>
2025-10-01 13:37:18 -0700

    wrapped index.js app with hash router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -7,7 +7,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
