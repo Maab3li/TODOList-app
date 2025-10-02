c3389bd000cb3b4777e9cf08c5c7c3801167b381
Maab3li <maab3li555@gmail.com>
2025-10-02 10:15:53 -0700

    updated basename value in index.js file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/TODOList-app">
         <App />
     </BrowserRouter>
 );
