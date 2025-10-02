cd2b6ef8f5a53744f42121e0bb37368268dc80ec
Maab3li <maab3li555@gmail.com>
2025-10-02 09:45:39 -0700

    removed route from app.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import TODOList from "./TODOList";
-import { Route, Routes } from "react-router-dom";
-import { Routes } from "react-router-dom";
 
 function App() {
   
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<TODOList />}>
-        </Route>
-      </Routes>
+      <TODOList />
     </div>
   )
 }
